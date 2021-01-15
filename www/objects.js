6e191bfaf5d90f461480ad9ac457e9700bdbb29d
Your <christophermoret@gmail.com>
2021-01-15 13:37:24 +0100

    Docker

---
+++ www/objects.js
@@ -0,0 +1,20 @@
+var joueur = {
+    pv: 10,
+    xp: 0,
+    force: 1,
+    endurance: 1,
+    arme: "Epée +1" ,//get dans arme
+    rubies: 20, 
+    id: 1
+  };
+
+  var PNJ = {
+    pv: 10,
+    xp: 0,
+    force: 1,
+    endurance: 1,
+    arme: "Epée +1" ,//get dans arme
+    rubies: 20
+  };
+
+  
