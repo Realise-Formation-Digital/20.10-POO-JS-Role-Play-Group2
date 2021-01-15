325bb9ca3a1193a363a6c2b5a2f263923897a63b
Dario <dariogallego2420@gmail.com>
2021-01-15 16:46:32 +0100

    fixed random

--- test.js
+++ test.js
@@ -12,28 +12,14 @@
             this.wpn = 1;
             this.gold = 20;
         }
-
-
-
-            //methods
-
-            fight() {
-                
-            }  
-        
-            run() {
-
-            }
-    }
-
-
-        
+            
+ 
+ }
+    
 
         //instance de la class joueur
         var joueur1 = new joueur('joueur1');
 
-
-    
     class NPC {
         constructor(name) {
             this.name = name;
@@ -45,5 +31,22 @@
             this.gold = 20;
 
         }
-    
     }
+
+
+    class monster {
+        constructor(name) {
+            this.name = name;
+            this.hp = Math.round(Math.random() * 50)+1;
+            this.xp = Math.round(Math.random() * 50)+1;
+            this.str = Math.round(Math.random() * 50)+1;
+            this.end = Math.round(Math.random() * 50)+1;
+            this.wpn = Math.round(Math.random() * 1);
+            this.gold = Math.round(Math.random() * 50)+1;
+        }
+
+    }
+        //instance de la class monstre
+        var monster1 = new monster('monster1');
+        
+
