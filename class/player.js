18b1ec0fae088eaca29b606d69b7b4607857900b
Marco Tribuzio <marco.tribuzio@realise.ch>
2021-01-20 09:56:47 +0100

    WIP POO - modified game js, index.html, player.js Optimization on game.js bugfix on player.js

--- class/player.js
+++ class/player.js
@@ -1,74 +1,78 @@
-    import Participant from './partecipant.js'
-    import Weapon from './weapon.js'
-    
-    // créer un joueur avec 10 points de vie, 0 points d'expérience, 1 point de force, une arme (qui est un créé avec la function Weapon et 2o piéces d'argent)
-    class Player extends Participant {
+import Participant from './partecipant.js'
+import Weapon from './weapon.js'
+import {startGame} from '../game.js'
 
-        constructor (name) {
-            var initialWeapon = new Weapon("Epee", 0, 1, 1);
-                super(name, 10, 0, 1, 1, [initialWeapon], 20)      // créer le joueur en utilisant le constructor de la clase Participant
-        } 
+// créer un joueur avec 10 points de vie, 0 points d'expérience, 1 point de force, une arme (qui est un créé avec la function Weapon et 2o piéces d'argent)
+class Player extends Participant {
+
+  constructor(name) {
+    const initialWeapon = new Weapon("Epee", 0, 1, 1);
+    super(name, 10, 0, 1, 1, [initialWeapon], 20)      // créer le joueur en utilisant le constructor de la clase Participant
+  }
 
 
-       //methods
-        show() {
-            document.getElementById("inventory").innerHTML = "Name: "+ this._name + "<br>HP: " + this._hp + "<br>XP: " + this._xp + "<br>STR: " + this._str + "<br>END: " + this._end + "<br>Gold: " + this._gold;
-        }
+  //methods
+  show() {
+    console.log("[Player][show] Showing player")
+    document.getElementById("inventory").innerHTML = "Name: " + this._name + "<br>HP: " + this._hp + "<br>XP: " + this._xp + "<br>STR: " + this._str + "<br>END: " + this._end + "<br>Gold: " + this._gold;
+  }
 
-        /**
-         * 
-         * @param {Object} monster - Monster angainst we want to fight
-         */
-        fight(monster) {
-            console.log("[Player][fight] Player fight against monster", monster)
-            let fightPlayer = (this._hp + this._xp + this._end + this._str);
-            let fightMonster = (monster._hp + monster._xp + monster._end + monster._str);
-        if (fightPlayer >= fightMonster) {
-                this._xp += monster._xp;
-                this._gold = this._gold + monster._gold;
-                console.log("win");
-        } else {
-            console.log("lose");
-            this._hp -= 1;
-            if(this.dead()) {
-                consola.log("You Died");
-                startGame(); }
-            }
-        }
-        run() {
-           // function fuir
-        }
-
-        // function mourir
-        dead () {
-            return this._hp === 0
-        }
-
-        buyWeapon(weapon) {
-           // function acheter une arme
-        }
-
-        sellWeapon(weapon, pnj) {
-           // function vendre une arme
-        }
-
-        takeWeapon(weapon) {
-           // function équiper une arme
-
-        } 
-
-        leaveWeapon(weapon) {
-            // function déséquiper une arme
-        }
-
-        byStrength() {
-            // function échanger 100 sous contre un point de force
-        }
-
-        buyEndurance() {
-            // function échanger 100 sous contre un point d'endurance
-        }
-
+  /**
+   *
+   * @param {Object} monster - Monster angainst we want to fight
+   */
+  fight(monster) {
+    console.log("[Player][fight] Player fight against monster", monster)
+    let fightPlayer = (this._hp + this._xp + this._end + this._str);
+    let fightMonster = (monster._hp + monster._xp + monster._end + monster._str);
+    if (fightPlayer >= fightMonster) {
+      this._xp += monster._xp;
+      this._gold = this._gold + monster._gold;
+      console.log("win");
+    } else {
+      console.log("lose");
+      this._hp -= 1;
+      if (this.dead()) {
+        console.log("You Died");
+        startGame();
+      }
     }
+  }
 
-    export default Player
+  run() {
+    // function fuir
+  }
+
+  // function mourir
+  dead() {
+    return this._hp === 0
+  }
+
+  buyWeapon(weapon) {
+    // function acheter une arme
+  }
+
+  sellWeapon(weapon, pnj) {
+    // function vendre une arme
+  }
+
+  takeWeapon(weapon) {
+    // function équiper une arme
+
+  }
+
+  leaveWeapon(weapon) {
+    // function déséquiper une arme
+  }
+
+  byStrength() {
+    // function échanger 100 sous contre un point de force
+  }
+
+  buyEndurance() {
+    // function échanger 100 sous contre un point d'endurance
+  }
+
+}
+
+export default Player
--- game.js
+++ game.js
@@ -3,75 +3,116 @@ import Player from './class/player.js'
 import Monster from './class/monster.js'
 import NPC from './class/npc.js'
 
-const textElement = document.getElementById('text')
-const optionButtonsElement = document.getElementById('option-buttons')
-
 let state = {}
+let player1 = null
+let monster1 = null
 
-function startGame() {
+/**
+ * Function that start the game
+ */
+export function startGame() {
+  console.log("[Game][StartGame] Starting game")
+
+  // let player1 = null
+  // let player2 = null
+  // let monster1 = null
+  let monster2 = null
+  let monster1weapon = null
+  let npcWeaponFlecha = null
+  let npcWeaponMisil = null
+
   state = {}
   showTextNode(1);
 
-//instance de la class joueur
-var player = new Player('Player1');
-var player2 = new Player('Player2');
+  player1 = new Player('Player1'); //instance de la class joueur
+  player1.show();
+  monster1weapon = new Weapon("Espada", 30); // instance of the weapone for the monster 1
+  monster1 = new Monster("Monstre1", monster1weapon); // new monster with weapon created
+  // monster2 = new Monster("Monstre2", null); // new monster without weapon
+  player1.fight(monster1) // play against monster 1
+  npcWeaponFlecha = new Weapon("Flecha", 10) // new weapon for npc
+  npcWeaponMisil = new Weapon("Misil", 50) // new weapon for npc
+  const npc = new NPC([npcWeaponFlecha, npcWeaponMisil]); // new instance of npc with two weapon already declared
 
-player.show();
-  
-//instance de la class nonstre avec 2 paramètres: name du monstre et array arme (name, prix) OU name et array null si le monstre n'a pas d'arme
-let monster = new Monster("Monstre1", new Weapon("Espada", 30));
-var monster1 = new Monster("Monstre2", null);
-  
-player.fight(monster)
-
-// instance de la clase pnj avec 2 armes (chaque arme a un name et un prix)
-var npc = new NPC([new Weapon("Flecha", 10), new Weapon("Misil", 50)]);
-      
 }
 
-
+/**
+ * Show the right envriroment
+ * @param {Number} textNodeIndex - index to search
+ */
 function showTextNode(textNodeIndex) {
+  console.log("[Game][ShowTextNode] Show text node with params", textNodeIndex)
+  let textElement = null
+  let textNode = null
+  let optionButtonsElement = []
 
-    //replace text element with textNode
-  const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+  textElement = document.getElementById('text')
+  if (!textElement) console.error("Tag not found")
+
+  optionButtonsElement = document.getElementById('option-buttons')
+  if (optionButtonsElement.length === 0) console.error("No buttons found")
+
+  //replace text element with textNode
+  textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+  if (!textNode) console.error("No node found")
+  if (textNode && !textNode.options) console.log("No Option found for a given text node")
   textElement.innerText = textNode.text
 
   // remove all the options
   while (optionButtonsElement.firstChild) {
     optionButtonsElement.removeChild(optionButtonsElement.firstChild)
   }
+
   // add options from nodes
   textNode.options.forEach(option => {
-    if (showOption(option)) {
+    //if (showOption(option)) {
       const button = document.createElement('button')
       button.innerText = option.text
       button.classList.add('btn')
       button.addEventListener('click', () => selectOption(option))
       optionButtonsElement.appendChild(button)
-    }
+    //}
   })
 }
 
 function showOption(option) {
-  return option.requiredState == null || option.requiredState(state)
+  return option.requiredState === null || option.requiredState(state)
 }
 
+/**
+ *
+ * @param option
+ */
 function selectOption(option) {
+  console.log("[Game][selectOption] Show select option with params", option)
   const nextTextNodeId = option.nextText
   //if (nextTextNodeId <= 0) {
- //   return startGame() }
- if (nextTextNodeId == 5) {
-  player._gold -=20;
-  player.show();
- } else if(nextTextNodeId == 6) {
-  player._gold +=20;
-  player.show();
- }
+  //   return startGame() }
+
+  switch (nextTextNodeId) {
+    case 5: {
+      player1._gold -= 20;
+      player1.show();
+      break
+    }
+    case 6: {
+      player1._gold += 20;
+      player1.show();
+      break
+    }
+  }
+
   state = Object.assign(state, option.setState)
+  console.log("State", state)
   showTextNode(nextTextNodeId)
 }
 
+/**
+ *
+ * @type {({options: [{nextText: number, text: string}, {nextText: number, text: string}, {nextText: number, text: string}], id: number, text: string}|{options: [{nextText: number, text: string}, {nextText: number, text: string}, {nextText: number, text: string}], id: number, text: string}|{options: [{nextText: number, text: string}, {nextText: number, text: string}, {nextText: number, text: string}], id: number, text: string}|{id: number, text: string}|{options: [{nextText: number, text: string}, {nextText: number, text: string}], id: number, text: string})[]}
+ */
 const textNodes = [
+
   {
     id: 1,
     text: 'Welcome.. Got a selection of good things on sale, stranger!',
@@ -90,6 +131,7 @@ const textNodes = [
       }
     ]
   },
+
   {
     id: 2,
     text: "What're ya buyin?",
@@ -108,6 +150,7 @@ const textNodes = [
       }
     ]
   },
+
   {
     id: 3,
     text: "What're ya sellin'?",
